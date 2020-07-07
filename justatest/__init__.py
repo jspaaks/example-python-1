33c04cb8d5439e1f238524e01ba6f8aea8cbf404
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2020-07-07 17:35:54 +0200

    simple test of self hosted runner with pythomn

--- justatest/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.1.0"
-
-
-def sum(a, b):
-    """Something"""
-    return a + b
--- tests/test_justatest.py
+++
@@ -1,9 +0,0 @@
-from justatest import __version__, sum
-
-
-def test_version():
-    assert __version__ == "0.1.0"
-
-
-def test_sum():
-    assert sum(2, 4) == 6
